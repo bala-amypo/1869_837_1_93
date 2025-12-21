59eda0c2c1fb1abd60a79b45c7c5072478cd1d0d
bala-amypo <bala@amypo.in>
2025-12-21 17:10:34 +0000

    User commit

---
+++ src/main/java/com/example/controller/RuleController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.InteractionRule;
+import com.example.demo.service.RuleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rules")
+public class RuleController {
+
+    private final RuleService ruleService;
+
+    public RuleController(RuleService ruleService) {
+        this.ruleService = ruleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<InteractionRule> addRule(
+            @RequestBody InteractionRule rule) {
+
+        return ResponseEntity.ok(ruleService.addRule(rule));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<InteractionRule>> getAllRules() {
+
+        return ResponseEntity.ok(ruleService.getAllRules());
+    }
+}
