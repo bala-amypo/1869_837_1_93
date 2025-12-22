54cc764bfceb0739d33054530018cd64beed5ae0
bala-amypo <bala@amypo.in>
2025-12-22 03:18:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/service /impl/CatalogServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.ActiveIngredient;
+import com.example.demo.model.Medication;
+import com.example.demo.repository.ActiveIngredientRepository;
+import com.example.demo.repository.MedicationRepository;
+import com.example.demo.service.CatalogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CatalogServiceImpl implements CatalogService {
+
+    private final ActiveIngredientRepository ingredientRepository;
+    private final MedicationRepository medicationRepository;
+
+    public CatalogServiceImpl(ActiveIngredientRepository ingredientRepository,
+                              MedicationRepository medicationRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.medicationRepository = medicationRepository;
+    }
+
+    @Override
+    public ActiveIngredient addIngredient(ActiveIngredient ingredient) {
+
+        if (ingredientRepository.existsByName(ingredient.getName())) {
+            throw new IllegalArgumentException("Ingredient already exists");
+        }
+
+        return ingredientRepository.save(ingredient);
+    }
+
+    @Override
+    public Medication addMedication(Medication medication) {
+
+        if (medication.getIngredients() == null ||
+                medication.getIngredients().isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Medication must contain at least one ingredient");
+        }
+
+        return medicationRepository.save(medication);
+    }
+
+    @Override
+    public List<Medication> getAllMedications() {
+        return medicationRepository.findAll();
+    }
+}
