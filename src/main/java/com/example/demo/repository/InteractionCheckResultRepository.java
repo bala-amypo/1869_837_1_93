32bc086b46c6abba2f109281b50455b874d87a62
bala-amypo <bala@amypo.in>
2025-12-23 03:32:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/InteractionCheckResultRepository.java
+++ src/main/java/com/example/demo/repository/InteractionCheckResultRepository.java
@@ -3,7 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.InteractionCheckResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InteractionCheckResultRepository
-        extends JpaRepository<InteractionCheckResult, Long> {
-    // save, findById, findAll are sufficient
+public interface InteractionCheckResultRepository extends JpaRepository<InteractionCheckResult, Long> {
 }
