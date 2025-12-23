8bbe0ac44907b314c59f2653d8b2643d387271f3
bala-amypo <bala@amypo.in>
2025-12-23 01:24:16 +0000

    User commit

---
+++ src/main/java/com/example/repository/InteractionCheckResultRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.InteractionCheckResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InteractionCheckResultRepository
+        extends JpaRepository<InteractionCheckResult, Long> {
+    // save, findById, findAll are sufficient
+}
---
+++ src/main/java/com/example/repository/MedicationRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicationRepository
+        extends JpaRepository<Medication, Long> {
+    // Standard CRUD is sufficient
+}
