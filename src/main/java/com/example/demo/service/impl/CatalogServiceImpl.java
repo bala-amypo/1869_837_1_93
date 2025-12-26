2ccee200e4bcf9481d71c3675ea19dfacaf18fb1
bala-amypo <bala@amypo.in>
2025-12-26 06:42:27 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
@@ -1,15 +1,48 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
-import com.example.demo.model.InteractionCheckResult;
+import com.example.demo.model.ActiveIngredient;
+import com.example.demo.model.Medication;
+import com.example.demo.repository.ActiveIngredientRepository;
+import com.example.demo.repository.MedicationRepository;
+import com.example.demo.service.CatalogService;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-public interface InteractionService {
+@Service
+public class CatalogServiceImpl implements CatalogService {
 
-    void checkInteractions(List<Long> medicationIds);
-    String getResult(Long resultId);
+    private final MedicationRepository medicationRepository;
+    private final ActiveIngredientRepository ingredientRepository;
 
-    // Required by Evaluation & Result Controllers
-    InteractionCheckResult save(InteractionCheckResult result);
-    InteractionCheckResult findById(Long id);
-    List<InteractionCheckResult> findAll();
+    public CatalogServiceImpl(MedicationRepository medicationRepository,
+                              ActiveIngredientRepository ingredientRepository) {
+        this.medicationRepository = medicationRepository;
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public void addIngredient(ActiveIngredient ingredient) {
+        ingredientRepository.save(ingredient);
+    }
+
+    @Override
+    public void addMedication(Medication medication) {
+        medicationRepository.save(medication);
+    }
+
+    @Override
+    public Medication saveMedication(Medication medication){
+        return medicationRepository.save(medication);
+    }
+
+    @Override
+    public Medication getMedicationById(Long id){
+        return medicationRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Medication> getAllMedications(){
+        return medicationRepository.findAll();
+    }
 }
