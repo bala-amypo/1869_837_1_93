669bc1e32521218b26b4996314b8c240b02627a4
bala-amypo <bala@amypo.in>
2025-12-26 08:01:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/InteractionRuleRepository.java
+++ src/main/java/com/example/demo/repository/InteractionRuleRepository.java
@@ -2,12 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.InteractionRule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InteractionRuleRepository extends JpaRepository<InteractionRule, Long> {
 
-    InteractionRule findRuleBetweenIngredients(Long a, Long b);
-
-    InteractionRule findByIngredientId(Long id);
-
-    InteractionRule findById(long id);
+    InteractionRule findByIngredientAAndIngredientB(Long ingredientA, Long ingredientB);
 }
