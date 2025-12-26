2bb3ca020c43bb34acd23ac60294c6f167360924
bala-amypo <bala@amypo.in>
2025-12-26 04:03:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,19 +1,56 @@
 package com.example.demo.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import com.example.demo.util.JwtUtil;
+import org.springframework.security.authentication.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 @RestController
-@RequestMapping("/api/auth")
-@Tag(name = "Auth")
+@RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager,
+                          UserService userService,
+                          JwtUtil jwtUtil,
+                          PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // REGISTER API
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        return userService.register(user);
+    }
+
+    // LOGIN API
     @PostMapping("/login")
-    @Operation(summary = "Login (dummy, no JWT)")
-    public Map<String, String> login(@RequestBody Map<String, String> body) {
-        return Map.of("message", "Login successful (JWT disabled)");
+    public String login(@RequestBody User user) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getEmail(),
+                        user.getPassword()
+                )
+        );
+
+        User dbUser = userService.findByEmail(user.getEmail());
+
+        return jwtUtil.generateToken(
+                dbUser.getEmail(),
+                dbUser.getId(),
+                dbUser.getRole()
+        );
     }
 }
